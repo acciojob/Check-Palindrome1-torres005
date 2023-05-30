12234ce6131c328bc0387a828eb914e9357230f4
torres005 <109883423+torres005@users.noreply.github.com>
2023-05-30 16:22:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
+     var word = str.spilit("");
+	var firstLetter = word.shift();
+	var lastLetter = word.pop();
 
+	if(firstLetter! == lastLetter){
+		console.log("false");
+	}
+	else {
+		console.log("true");
+	}
 }
 module.exports = palindrome
