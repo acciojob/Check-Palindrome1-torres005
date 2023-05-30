4c3974040856d15d9b0c21d1f3a285b6d63df836
torres005 <109883423+torres005@users.noreply.github.com>
2023-05-30 16:38:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-// complete the given function
+// complete t given function
 
-function palindrome(str){
-     var word = str.spilit("");
-	var firstLetter = word.shift();
-	var lastLetter = word.pop();
+function checkPalindrom(palindrom)
+{
 
-	if(firstLetter! == lastLetter){
-		console.log("false");
-	}
-	else {
-		console.log("true");
-	}
+    for( var i = palindrom.length; i > 0; i-- )
+    {
+        if( palindrom[i] = palindrom.charAt(palindrom.length)-1 )
+        {
+            document.write('the word is palindrome.');
+        }else{
+            document.write('the word is not palindrome!');
+        }
+    }
 }
+// checkPalindrom('wordthatwillbechecked');
 module.exports = palindrome
